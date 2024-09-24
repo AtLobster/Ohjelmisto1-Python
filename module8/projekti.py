18bb82356e3c0e6416d1cf4e6223d7e5a895852d
Atte Sten <arafsambi@gmail.com>
2024-09-24 19:14:19 +0300

    ----

--- module8/projekti.py
+++ module8/projekti.py
@@ -3,6 +3,7 @@ import databases
 from geopy.distance import great_circle
 
 def randomAirport():
+
     sql = """
     SELECT airport.name, country.name, latitude_deg, longitude_deg 
     FROM airport 
@@ -13,43 +14,63 @@ def randomAirport():
     #Valitaan satunnainen lentokenttä ja maa, jossa lentokenttä sijaitsee
     cursor = databases.conn.cursor()
     cursor.execute(sql)
-    randAirport = cursor.fetchall()
 
     #Halutaan ulos satunnaisen lentokentän nimi ja maa
+    randAirport = cursor.fetchall()
     airportName = randAirport[0][0]
     countryName = randAirport[0][1]
-    startPointLat = randAirport[0][2]
-    startPointLon = randAirport[0][3]
+    startPoint = (randAirport[0][2], randAirport[0][3])
 
     #print(airportName, countryName)
-    return airportName, countryName, startPointLat, startPointLon
+    return airportName, countryName, startPoint
 
 def wrongCountry(exceptionCountry):
+
     sql = """
-    SELECT DISTINCT country.name
+    SELECT DISTINCT country.name, latitude_deg, longitude_deg
     from country
     JOIN airport ON country.iso_country = airport.iso_country
     WHERE airport.type LIKE 'large_airport%' AND country.name != '{exceptionCountry}'
     ORDER BY rand()
     LIMIT 5
     """
-
     cursor = databases.conn.cursor()
     cursor.execute(sql)
+
     wrongCountries = cursor.fetchall()
+    endPoints = (wrongCountries[0][1], wrongCountries[0][2])
 
+    return [(country[0], country[1], country[2]) for country in wrongCountries]
 
-    return [country[0] for country in wrongCountries]
+def distance():
 
+    alku = (40.7128, -74.0060)
+    loppu = (34.0522, -118.2437)
 
-airportName, countryName, startPointLat, startPointLon = randomAirport()
-muuMaaMustikka = wrongCountry(countryName)
+    dist = great_circle(alku, loppu).kilometers
+    print(dist)
 
-countryChoices = [countryName] + muuMaaMustikka
+    return dist
+
+airportName, countryName, startPoint = randomAirport()
+wNamesAndCoords = wrongCountry(countryName)
+wNames = [country[0] for country in wNamesAndCoords]
+
+countryChoices = [countryName] + wNames
 random.shuffle(countryChoices)
 
-print(countryName)
+startLat, startLon = startPoint
+print(f'Starting point: {airportName}, ({startLat}, {startLon})')
+
+for country in wNamesAndCoords:
+    destName = country[0]
+    destLat = country[1]
+    destLon = country[2]
+    print(f'End point: {destName}, ({destLat}, {destLon})')
+
 print(countryChoices)
+
+
 
 """
 Pitää järjestää countryChoices lista random järjestyksesen ettei oikea 
