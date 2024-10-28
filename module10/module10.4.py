38b94fcb0e5526766656669ba485cfdddfec6c33
Atte Sten <arafsambi@gmail.com>
2024-10-28 13:15:48 +0200

    moduulien duunailuja

--- module10/module10.4.py
+++ module10/module10.4.py
@@ -2,23 +2,25 @@ import random
 
 
 class Auto: #Aika merkittävä tunneissa.
-    def __init__(self, rek, nopeus, aika=0):
+    def __init__(self, rek, nopeus):
         self.rek = rek
         self.nopeus = nopeus
-        self.kMatka = nopeus * aika
+        self.kMatka = 0
         self.huippuNopeus = 142
 
     def kiihdyta(self, deltaNopeus):
+        uusi_nopeus = self.nopeus + deltaNopeus
+        if uusi_nopeus > self.huippuNopeus:
+            self.nopeus = self.huippuNopeus
+        elif uusi_nopeus < 0:
+            self.nopeus = 0
+        else:
+            self.nopeus = uusi_nopeus
 
-        if self.nopeus + deltaNopeus <= self.huippuNopeus:
-            self.nopeus += deltaNopeus
-            if self.nopeus + deltaNopeus < 0:
-                self.nopeus = 0
-            return
 
     def kulje(self, aika):
         self.kMatka += aika * self.nopeus
-        return
+
 
 class Kilpailu:
     def __init__(self, nimi, pituus, autot):
@@ -45,25 +47,35 @@ class Kilpailu:
 
 
 
-autot = []
+def luo_autot(maara):
+    autot = []
+    for i in range(1, maara + 1):
+        rek = f"Auto{i}"
+        huippuNopeus = random.randint(100, 200)
+        auto = Auto(rek, 0)
+        auto.huippuNopeus = huippuNopeus
+        autot.append(auto)
+    return autot
 
-for i in range(1, 11):
-    rek = f"Auto{i}"
-    huippuNopeus = random.randint(100,200)
-    auto = Auto(rek, 0)
-    auto.huippuNopeus = huippuNopeus
-    autot.append(auto)
+def main():
+    kilpailu_nimi = "Suuri romuralli"
+    kilpailu_pituus = 8000
+    autojen_maara = 10
+
+    autot = luo_autot(autojen_maara)
+
+    kilpailu = Kilpailu(kilpailu_nimi, kilpailu_pituus, autot)
+
+    tunnit = 0
+    while not kilpailu.kilpailu_ohi():
+        kilpailu.tunti_kuluu()
+        tunnit += 1
+        if tunnit % 10 == 0:
+            print(f"Tunnin {tunnit} tilanne: ")
+            kilpailu.tulosta_tilanne()
+
+    print("Kilpailu on päättynyt!")
+    kilpailu.tulosta_tilanne()
 
 
-kilpailu = Kilpailu("Suuri romuralli", 8000, autot)
-
-tunnit = 0
-while not kilpailu.kilpailu_ohi():
-    kilpailu.tunti_kuluu()
-    tunnit += 1
-    if tunnit % 10 == 0:
-        print(f"Tunnin {tunnit} tilanne: ")
-        kilpailu.tulosta_tilanne()
-
-print("Kilpailu on päättynyt!")
-kilpailu.tulosta_tilanne()
+main()
